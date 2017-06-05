022ace520edef875599677534c530db343719b8f
Unknown <dawidsonb95@gmail.com>
2017-06-05 20:52:57 +0200

    Model Rate, Entity configuration for Rate

---
+++ FriGo/FriGo.Db/EntityConfigurations/RateConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using FriGo.Db.Models.Recipes;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FriGo.Db.EntityConfigurations
+{
+    class RateConfiguration : EntityTypeConfiguration<Rate>
+    {
+        public RateConfiguration()
+        {
+            HasRequired(rate => rate.User) 
+                    .WithMany(user => user.Rates);  
+        }
+    }
+}
---
+++ FriGo/FriGo.Db/Models/Recipes/Rate.cs
@@ -0,0 +1,16 @@
+﻿using FriGo.Db.Models.Authentication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FriGo.Db.Models.Recipes
+{
+    class Rate : Entity
+    {
+        public int Rating { get; set; }
+        public virtual Recipe Recipe { get; set; }
+        public virtual User User { get; set; }
+    }
+}
